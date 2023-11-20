7a36c5a953f0d0e9e6ae04bf79878c13b5d050dd
yuto-0226 <wayne71112@gmail.com>
2023-11-20 17:00:48 +0800

    setup bs4 and requests

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
+import requests
 from bs4 import BeautifulSoup
+
+url = "https://www.ptt.cc/bbs/TTU-talk/index.html"
+web = requests.get(url)                        # 取得網頁內容
+soup = BeautifulSoup(web.text, "html.parser")  # 轉換成標籤樹
+print(soup)
