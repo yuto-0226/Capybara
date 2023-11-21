25f343157aa2e35e3d63ef049e933204b51ae37d
yuto-0226 <wayne71112@gmail.com>
2023-11-21 15:43:30 +0800

    setup architecture

---
+++ crawler/crawler.py
@@ -0,0 +1,11 @@
+from crawler import *
+
+def main():
+    url = "https://www.ptt.cc/bbs/TTU-talk/index.html"
+    web = requests.get(url)                        # 取得網頁內容
+    soup = BeautifulSoup(web.text, "html.parser")  # 轉換成標籤樹
+    print(soup)
+
+# 若執行進入點為此檔案則執行以下
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,7 +1,2 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.ptt.cc/bbs/TTU-talk/index.html"
-web = requests.get(url)                        # 取得網頁內容
-soup = BeautifulSoup(web.text, "html.parser")  # 轉換成標籤樹
-print(soup)
+from crawler import crawler
+crawler.main()
