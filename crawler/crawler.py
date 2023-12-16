8895b86cd835ead9a8fa392d00af73f1b93f315b
yuto-0226 <wayne71112@gmail.com>
2023-12-16 22:36:46 +0800

    add query() function

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,15 +1,38 @@
 from crawler import *
 
-class PTTcrawler():
-    def __init__():
-        root = "https://www.ppt.cc"
+root = "https://www.ptt.cc"
+
+# 回傳一個 url 字串
+# args: 版面, 關鍵字(list), 頁數
+def query(board: str, keywords: list, page: int) -> str:
+    url = root
+    url += f"/bbs/{board}/"
+    
+    if keywords is not None:
+        url+="search?q="
+        for words in keywords:
+            url+=words
+            if words != keywords[-1]:
+                url+="+"
+    
+    if page is not None:
+        if keywords is not None:
+            url+=f"&page={page}"
+        else:
+            url+=f"page={page}"
+    
+    return url
 
 def main():
-    url = "https://www.ptt.cc/bbs/TTU-talk/index.html"
+    url = query("TTU-talk",["資工"],1)
+    print(url)
     web = requests.get(url)                        # 取得網頁內容
     soup = BeautifulSoup(web.text, "html.parser")  # 轉換成標籤樹
     print(soup)
 
 # 若執行進入點為此檔案則執行以下
 if __name__ == "__main__":
+    import requests
+    import requests_html
+    from bs4 import BeautifulSoup
     main()
